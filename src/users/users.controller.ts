ab589dcb37d9891a374f96e1b7a47430a78a7aba
dokesz <lacivagyok3@gmail.com>
2024-03-19 12:00:51 +0100

    simple nest.js test project with docker compose

---
+++ src/users/users.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return await this.userService.findall();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<User> {
+    const user = await this.userService.findOne(id);
+    if (!user) {
+      throw new Error(' User was not found');
+    } else {
+      return user;
+    }
+  }
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return await this.userService.create(user);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: User): Promise<User> {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    const user = await this.userService.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    } else {
+      return this.userService.delete(id);
+    }
+  }
+}
