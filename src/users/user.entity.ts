6f499c534c6d8e2a1addb13437f28759f2b71b6e
dokesz <lacivagyok3@gmail.com>
2024-03-21 15:44:59 +0100

    changed .prettier

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
-
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 }
